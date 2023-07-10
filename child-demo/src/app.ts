c07f27fd25f64f588caea2d1d39e225c14990ea5
liangchaofei <chaofei_liang@126.com>
2023-07-10 10:54:07 +0800

    feat: child

---
+++ child-demo/src/app.ts
@@ -0,0 +1,34 @@
+// 运行时配置
+
+// 全局初始化数据配置，用于 Layout 用户信息和权限初始化
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: '@umijs/max' };
+}
+
+export const layout = () => {
+  return {
+    logo: 'https://img.alicdn.com/tfs/TB1YHEpwUT1gK0jSZFhXXaAtVXa-28-27.svg',
+    menu: {
+      locale: false,
+    },
+  };
+};
+
+export const qiankun = {
+    // 应用加载之前
+    async bootstrap(props: any) {
+      console.log('-----------test bootstrap', props);
+    },
+    // 应用 render 之前触发
+    async mount(props: any) {
+      console.log('------------test mount', props);
+    },
+    async update(props: any) {
+      console.log('------------test update', props);
+    },
+    // 应用卸载之后触发
+    async unmount(props: any) {
+      console.log('------------test unmount', props);
+    }
+  };
